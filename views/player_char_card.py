28cb70b18e1969dee42c287db847993e66b076b4
Mortos Der Soulstealer <iAmMortos@users.noreply.github.com>
2020-07-12 01:59:43 -0600

    player_char_card test
    Added player_char_card test entry point for testing progress

--- views/player_char_card.py
+++ views/player_char_card.py
@@ -28,20 +28,3 @@ class PlayerCharCard(ui.View):
     v.init(player, char)
     return v
 
-
-if __name__ == '__main__':
-  from characters import Characters
-  import random
-  v = ui.View()
-  _,_,w,h = v.frame
-  cs = Characters().list_alpha()
-  players = ['Father1337', 'Valfor']
-  cards = []
-  for i, player in enumerate(players):
-    char = random.choice(cs)
-    cs.remove(char)
-    card = PlayerCharCard.load_view(player, char)
-    card.flex = 'WHLRTB'
-    card.frame = (i*(w/len(players)), 0, w/len(players), h)
-    v.add_subview(card)
-  v.present(orientations=['landscape'], hide_title_bar=True)
